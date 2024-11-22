cd75326745c38fb2611caf6d93e220790bb7cc6a
VC2008 <526Vcolon@frhsd.com>
2024-11-22 09:30:40 -0500

    Everything is broken oh no oh gawd

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ console.log(adjective)
 console.log(pronoun)
 console.log(conjunction)
 
-let myStory = `Hello ${username}, how are you doing today? can you believe that you're ${userAge} years old? you're growing up so fast and im proud of you. You hungry at all? ${yesorno}. I see, well if you're itching for some real delicacies, then I have something for you.
-A good food to start the day off with is ${noun}`
-console.log(myStory)
+let myStory = `Hello ${username}, how are you doing today? can you believe that you're ${userAge} years old? you're growing up so fast, thats very nice. You hungry at all? ${yesorno}.
+ I see, well if you're itching for some serious delicacies, then I have something interesting for you. This my favorite brand of word salad! Words such as ${Noun} make some great bites to eat during the day and are rich in Vitamin C.` 
 
-document.getElementById('story').innerHTML = myStory
+console.log(myStory)
+document.getElementById('story').innerHTML = mystory;
+
